afa231fe4a56d98f6a71eebff9fdf6ec8d89d843
Winnie Lin <onecentlin@gmail.com>
2024-03-31 22:57:27 +0800

    Add Panel component

--- NetAppWithVite/Assets/js/components/App.vue
+++ NetAppWithVite/Assets/js/components/App.vue
@@ -1,6 +1,19 @@
 <script setup>
-    
+    import Panel from './Panel.vue';
 </script>
 <template>
     <h2>Hello .NET Core with Vite integrated</h2>
+
+    <Panel title="Message Title">
+        Message Shows Here
+
+        <template #footer>
+            This is footer info
+        </template>
+    </Panel>
+
+    <Panel title="Message 2">
+        Message without footer slot info
+    </Panel>
+
 </template>
---
+++ NetAppWithVite/Assets/js/components/Panel.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps({
+        title: String
+    });
+</script>
+<template>
+    <div class="card bg-gradient my-3">
+        <h4 class="card-header">{{ props.title }}</h4>
+        <div class="card-body">
+            <slot></slot>
+        </div>
+        <div class="card-footer" v-if="$slots.footer">
+            <slot name="footer"></slot>
+        </div>
+    </div>
+</template>
