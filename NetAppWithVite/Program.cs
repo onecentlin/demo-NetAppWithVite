27390fdfd9de959c84c5c64dcdb81a092c9edc5a
Winnie Lin <onecentlin@gmail.com>
2024-03-31 15:00:14 +0800

    Integrate Vite in development environment

--- NetAppWithVite/Program.cs
+++ NetAppWithVite/Program.cs
@@ -1,9 +1,23 @@
+using Vite.AspNetCore.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// [Vite] Add Vite services
+builder.Services.AddViteServices(options =>
+{
+    options.Server.AutoRun = true;
+});
+
 var app = builder.Build();
+
+// [Vite] Use Vite development server in development environment
+if (app.Environment.IsDevelopment())
+{
+    app.UseViteDevelopmentServer(true);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
